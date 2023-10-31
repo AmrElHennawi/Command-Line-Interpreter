1fe7e36075d1235729878477442cb86e79ead07f
Yusuf Badr <yusufbadr@yahoo.com>
2023-10-31 20:35:46 +0200

    basic structure

---
+++ Parser.java
@@ -0,0 +1,16 @@
+public class Parser {
+    String commandName;
+    String[] args;
+
+    public String getCommandName() {
+        return commandName;
+    }
+
+    public String[] getArgs() {
+        return args;
+    }
+
+    public boolean parse (String input){
+        return false;
+    }
+}
---
+++ Terminal.java
@@ -0,0 +1,21 @@
+public class Terminal {
+    Parser parser;
+
+    public String pwd(){
+        return " ";
+    }
+
+    public void cd(String[] args){
+
+    }
+
+    public void chooseCommandAction(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+
+}
