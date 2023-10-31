4f8374971add2bea7ffcdcb07e2aa48a9d968f9b
Yusuf Badr <yusufbadr@yahoo.com>
2023-10-31 23:17:42 +0200

    implemented mkdir

--- Terminal.java
+++ Terminal.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 public class Terminal {
     Parser parser;
 
@@ -9,6 +11,26 @@ public class Terminal {
 //    public void cd(String[] args){
 //
 //    }
+
+    public void mkdir(String[] args){
+        for (String path : args){
+            File directory = new File(path);
+            if (!directory.exists()){
+                boolean success = directory.mkdirs();
+                if (success){
+                    System.out.println("Directory created successfully at " + directory.getAbsolutePath());
+                } else {
+                    System.err.println("Failed to create directory");
+                }
+            } else {
+                System.err.println("Directory already exists");
+            }
+        }
+    }
+
+    public void rmdir(String arg){
+
+    }
 
 
     // this method will choose the suitable command method to be called
