a0eb0b2f36dd5a1c0fd69c8544b8a0c60cc9e4c4
Yusuf Badr <yusufbadr@yahoo.com>
2023-11-01 16:49:10 +0200

    Finished rmdir

--- Terminal.java
+++ Terminal.java
@@ -1,6 +1,14 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class Terminal {
+    Path currentPath;
+    Terminal(){
+        currentPath = Paths.get(System.getProperty("user.dir"));
+    }
     Parser parser;
 
     // TODO: implement each command in a method, for example:
@@ -28,8 +36,39 @@ public class Terminal {
         }
     }
 
-    public void rmdir(String arg){
 
+    // precondition: arg is a directory
+    public void rmdir(String arg) throws IOException {
+        if (arg.equals("*")){
+            Files.walkFileTree(currentPath, new SimpleFileVisitor<Path>(){
+                public FileVisitResult afterVisitingDirectory(Path directory, IOException exception) throws IOException{
+                    if (Files.isDirectory(directory)){
+                        try(Stream<Path> directoryContents = Files.list(directory)){
+                            if (directoryContents.findAny().isEmpty()){
+                                Files.delete(directory);
+                            }
+                        }
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+        });
+        }
+        else {
+            Path filePath = Paths.get(arg);
+            try (Stream<Path> directoryContents = Files.list(filePath)){
+                if (directoryContents.findFirst().isPresent()){
+                    System.err.println("Directory not empty\n");
+                    return;
+                }
+            }
+
+            try{
+                Files.delete(filePath);
+                System.out.println("Directory deleted successfully");
+            } catch(IOException e){
+                System.err.println("Failed to delete the directory: " + e.getMessage());
+            }
+        }
     }
 
 
