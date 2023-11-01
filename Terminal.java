14a8ee053f26beeaa3a0be668721a956549c8a8a
mazen mahmoud <mazenmahmoudadly@gmail.com>
2023-11-01 17:37:14 +0200

    adding cp + pwd + rm + const home_dir + curr_dir

--- Terminal.java
+++ Terminal.java
@@ -1,12 +1,25 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
 
 public class Terminal {
     Parser parser;
+    // final => const , to keep home directory unchangable
+    private static final String HOME_DIRECTORY = System.getProperty("user.home");
+
+    private static String currentDirectory = HOME_DIRECTORY;
 
     // TODO: implement each command in a method, for example:
-//    public String pwd(){
-//        return " ";
-//    }
+
+   public String pwd(){
+       return currentDirectory;
+   }
 //
 //    public void cd(String[] args){
 //
@@ -31,6 +44,30 @@ public class Terminal {
     public void rmdir(String arg){
 
     }
+    public void cp(String[] arg){
+            Path sourcePath = Paths.get(currentDirectory, arg[0]);
+            Path destinationPath = Paths.get(currentDirectory, arg[1]);
+            try {
+                Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("File copied: " + sourcePath + " -> " + destinationPath);
+            } catch (IOException e) {
+                System.out.println("Failed to copy file: " + sourcePath + " -> " + destinationPath);
+            }
+        
+    }
+   
+    public void rm(String arg){
+             Path filePath = Paths.get(currentDirectory, arg);
+        try {
+            Files.delete(filePath);
+            System.out.println("File removed: " + filePath);
+        } catch (NoSuchFileException e) {
+            System.out.println("No such file!");
+        } 
+         catch (IOException e) {
+            System.out.println("Failed to remove file: " + filePath);
+        }
+    }
 
 
     // this method will choose the suitable command method to be called
@@ -39,8 +76,8 @@ public class Terminal {
     }
 
     public static void main(String[] args){
+        
+
 
     }
-
-
 }
