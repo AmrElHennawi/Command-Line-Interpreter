5cf644cb077db22a6353c3c14cc709edbac5aef4
Yusuf Badr <yusufbadr@yahoo.com>
2023-11-03 13:17:27 +0200

    finished parser function

--- Parser.java
+++ Parser.java
@@ -10,8 +10,26 @@ public class Parser {
         return args;
     }
 
-    // TODO: divide input to commandName and args
     public boolean parse (String input){
-        return false;
+        if (input.isEmpty()){
+            commandName = "";
+            args = new String[0];
+            return false;
+        }
+
+        String[] tokens = input.split("\\s+");
+
+        // at this point we are sure that there exists at least one token since the input string is not empty
+        commandName = tokens[0];
+
+        // if there exists more than one token then the rest are arguments
+        if (tokens.length > 1){
+            args = new String[tokens.length - 1];
+            System.arraycopy(tokens, 1, args, 0, args.length);
+        } else {
+            // if no more token, then arg is empty
+            args = new String[0];
+        }
+        return true;
     }
 }
